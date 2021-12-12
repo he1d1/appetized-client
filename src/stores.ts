d82abc24f631c9ee2996d11b893df814d2afb979
Lu <email@lewie.me>
2021-12-12 16:26:02 +0000

    Added `authed` and `user` stores
    - `authed` is a boolean containing weather a user is authenticated
    - `user` contains information about the currently logged in user.

--- src/stores.ts
+++ src/stores.ts
@@ -1,9 +1,14 @@
 import { writable } from 'svelte/store';
+// Reads config from ../config.json
 
 // Settings Object
 export interface Settings {
 	dark?: boolean;
 }
+
+export const config = writable({
+	SERVER_URL: 'http://localhost:4000'
+});
 
 // Object containing the user's preferences.
 export const settings = writable({
@@ -13,3 +18,20 @@ export const settings = writable({
 
 export const prefersDark = writable(false);
 export const dark = writable(true);
+
+export const authed = writable(false);
+export const user = writable({
+	id: undefined,
+	name: undefined,
+	username: undefined,
+	joinDate: undefined,
+	editDate: undefined,
+	profilePicture: undefined,
+	uploadedRecipes: undefined,
+	following: undefined,
+	followers: undefined,
+	savedRecipes: undefined
+});
+export const fab = writable(function () {
+	console.log('fab');
+});
