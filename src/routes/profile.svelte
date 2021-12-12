ec44264f40b2215f59e70c27e40bcaf591826331
Lu <email@lewie.me>
2021-12-12 16:27:08 +0000

    Added floating action button.

--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -1,1 +1,13 @@
-oeunstaohunteh
+<script>
+	import { onMount } from 'svelte';
+	import { fab, user } from '../stores';
+	import { goto } from '$app/navigation';
+
+	let text = 'nstaoheutoet uiod';
+
+	onMount(() => {
+		fab.set(() => goto('/settings#profile'));
+	});
+</script>
+
+{text}
