e1ca33e9d936b66d21d6a68181b6870301a714f6
Lu <email@lewie.me>
2021-12-19 16:53:49 +0000

    Made adaptor change based on ENV

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,25 @@
 import preprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
+import node from '@sveltejs/adapter-node';
+import dotenv from 'dotenv';
+dotenv.config();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
-		adapter: vercel(),
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: process.env.ADAPTER === 'VERCEL' ? vercel() : node({ out: 'out' })
+	},
+	ssr: true,
+	prerender: {
+		crawl: true,
+		enabled: true,
+		entries: ['*'],
+		onError: 'fail'
 	}
 };
 
