9907630271658eff3faea9cd90b3ff9f764d525e
Lu <email@lewie.me>
2022-02-27 12:47:35 +0000

    Added infinite scroll to homepage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,13 +17,16 @@
                 query: `
                     query Feed($id: String) {
                       recipes(
+                        take: 10
                         where: {
-                          author: {
-                            followers: {
-                              every: { id: { equals: $id } }
+                      author: {
+                          followers: {
+                            some: {
+                              id: {equals: $id}
                             }
                           }
                         }
+                      }
                         sort: {
                           createdAt: DESC
                         }
@@ -67,29 +70,104 @@
 
 <script>
     import {onMount} from "svelte";
-    import { currentRoute } from "../store"
+    import {currentRoute} from "../store"
     import Card from "$lib/Card.svelte";
 
     export let user, recipes;
 
+    let observer
     onMount(() => {
         $currentRoute = {
             name: 'Home',
             route: '/',
             buttons: []
         }
+
+        let loading = false;
+
+        observer = new IntersectionObserver(async (entries) => {
+            if (entries[0].isIntersecting) {
+                observer.unobserve(elements[elements.length - 3]);
+                await fetch('http://localhost:4000', {
+                    method: 'POST',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        query: `
+                            query Feed($id: String $from: ID) {
+                              recipes(
+                                take: 1
+                                from: $from
+                                skip: 1
+                                where: {
+                                author: {
+                          followers: {
+                            some: {
+                              id: {equals: $id}
+                            }
+                          }
+                        }
+                      }
+                        sort: {
+                          createdAt: DESC
+                        }
+                      ) {
+                        id
+                        name
+                        description
+                        author {
+                          id
+                          name
+                          username
+                          profilePicture {
+                            url
+                          }
+                        }
+                      }
+                    }
+                    `,
+                        variables: {
+                            id: user.id,
+                            from: recipes[recipes.length - 1].id
+                        }
+                    })
+                }).then((response) => response.json()).then((response => {
+                    if (response.errors) {
+                        return {
+                            error: response.errors[0].message,
+                            status: response.status
+                        };
+                    }
+
+                    if (response.data.recipes.length > 0) {
+                        recipes.push(response.data.recipes[0])
+                        recipes = recipes
+                    }
+
+                }))
+            }
+        })
+
     })
+
+    let elements = [];
+
+    $: observer?.observe(elements[elements.length - 3])
+    $: console.log(elements)
 
 </script>
 
 {#if recipes?.length}
     <div class="flex flex-col gap-4">
-        {#each recipes as recipe}
-            <a sveltekit:prefetch href={`/recipe/${recipe.id}`}>
+        {#each recipes as recipe, i}
+            <a class="recipe" sveltekit:prefetch href={`/recipe/${recipe.id}`} bind:this={elements[i]}>
                 <Card neutral>
                     <h2>{recipe.name}</h2>
                     {#if recipe?.description}<p>{recipe.description}</p>{/if}
-                    <a href={`/@${recipe.author.id}`} class="text-primary dark:text-primaryDark">{recipe.author.name ?? "@" + recipe.author.username}</a>
+                    <a href={`/@${recipe.author.id}`}
+                       class="text-primary dark:text-primaryDark">{recipe.author.name ?? "@" + recipe.author.username}</a>
                 </Card>
             </a>
         {/each}
